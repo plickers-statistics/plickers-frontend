db475ac4c9d33e0eb9d24718115d4d7950776d70
nvd05 <nvd05@bk.ru>
2024-12-21 18:36:45 +0500

    'App' => 'routers\home\Home'

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,10 @@
 
-import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { Routers } from './routers/Routers';
 
 
 test('renders learn react link', () => {
-	render(<App />);
+	render(<Routers />);
 	const linkElement = screen.getByText(/learn react/i);
 	expect(linkElement).toBeInTheDocument();
 });
